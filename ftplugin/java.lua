ad2b3ab4b22c96eed6aa9d33ec7fbcd49848e7f6
lucif3r2003 <buimanhtk1979@gmail.com>
2025-09-21 21:44:25 +0700

    fix the pain in the ass

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,36 +1,57 @@
+-- java.lua (fixed lombok handling)
 local home = os.getenv 'HOME'
 local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 local workspace_dir = workspace_path .. project_name
 
+-- Detect OS
+local os_name = vim.loop.os_uname().sysname:lower()
+local config_dir = os_name:match("darwin") and "config_mac" or "config_linux"
+
 local status, jdtls = pcall(require, 'jdtls')
 if not status then
+    vim.notify("jdtls not found!", vim.log.levels.ERROR)
     return
 end
+
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
 
+-- ✅ Better lombok path detection
+local lombok_path = home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar'
+local lombok_exists = vim.fn.filereadable(lombok_path) == 1
+
+-- Base JDTLS command
+local cmd = {
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+}
+
+-- ✅ Add lombok nếu có
+if lombok_exists then
+    table.insert(cmd, '-javaagent:' .. lombok_path)
+    vim.notify("Lombok.jar found - JDTLS will support @Data, @Getter, etc.", vim.log.levels.INFO)
+else
+    vim.notify("Lombok.jar not found - Install with: wget -O ~/.local/share/nvim/mason/packages/jdtls/lombok.jar https://projectlombok.org/downloads/lombok.jar", vim.log.levels.WARN)
+end
+
+-- Add JAR và config paths
+table.insert(cmd, '-jar')
+table.insert(cmd, vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'))
+table.insert(cmd, '-configuration')
+table.insert(cmd, home .. '/.local/share/nvim/mason/packages/jdtls/' .. config_dir)
+table.insert(cmd, '-data')
+table.insert(cmd, workspace_dir)
+
 local config = {
-    cmd = {
-        'java',
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-Xmx1g',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens',
-        'java.base/java.util=ALL-UNNAMED',
-        '--add-opens',
-        'java.base/java.lang=ALL-UNNAMED',
-        '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
-        '-jar',
-        vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-        '-configuration',
-        home .. '/.local/share/nvim/mason/packages/jdtls/config_mac',
-        '-data',
-        workspace_dir,
-    },
+    cmd = cmd,
     root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
 
     settings = {
@@ -48,11 +69,21 @@ local config = {
             },
             inlayHints = {
                 parameterNames = {
-                    enabled = 'all', -- literals, all, none
+                    enabled = 'all',
                 },
             },
             format = {
                 enabled = false,
+            },
+            -- Lombok support trong settings
+            configuration = {
+                runtimes = {
+                    {
+                        name = "JavaSE-21",
+                        path = "/usr/lib/jvm/java-21-openjdk", -- Adjust theo Java version của mày
+                        default = true,
+                    },
+                },
             },
         },
     },
@@ -60,14 +91,31 @@ local config = {
     init_options = {
         bundles = {},
     },
-}
-require('jdtls').start_or_attach(config)
 
-vim.keymap.set('n', '<leader>co', "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = 'Organize Imports' })
-vim.keymap.set('n', '<leader>crv', "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = 'Extract Variable' })
-vim.keymap.set('v', '<leader>crv', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
-    { desc = 'Extract Variable' })
-vim.keymap.set('n', '<leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
-vim.keymap.set('v', '<leader>crc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
-    { desc = 'Extract Constant' })
-vim.keymap.set('v', '<leader>crm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
+    on_attach = function(client, bufnr)
+        local opts = { buffer = bufnr, silent = true }
+        
+        -- JDTLS keymaps
+        vim.keymap.set('n', '<leader>co', "<Cmd>lua require'jdtls'.organize_imports()<CR>", 
+            vim.tbl_extend("force", opts, { desc = 'Organize Imports' }))
+        vim.keymap.set('n', '<leader>crv', "<Cmd>lua require('jdtls').extract_variable()<CR>", 
+            vim.tbl_extend("force", opts, { desc = 'Extract Variable' }))
+        vim.keymap.set('v', '<leader>crv', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", 
+            vim.tbl_extend("force", opts, { desc = 'Extract Variable' }))
+        vim.keymap.set('n', '<leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", 
+            vim.tbl_extend("force", opts, { desc = 'Extract Constant' }))
+        vim.keymap.set('v', '<leader>crc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", 
+            vim.tbl_extend("force", opts, { desc = 'Extract Constant' }))
+        vim.keymap.set('v', '<leader>crm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", 
+            vim.tbl_extend("force", opts, { desc = 'Extract Method' }))
+        
+        -- Test JDTLS connection
+        vim.notify("JDTLS attached to buffer " .. bufnr, vim.log.levels.INFO)
+    end,
+}
+
+-- Start JDTLS
+local status_ok, err = pcall(jdtls.start_or_attach, config)
+if not status_ok then
+    vim.notify("JDTLS failed to start: " .. tostring(err), vim.log.levels.ERROR)
+end
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,11 +19,10 @@ return {
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
-            local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             -- Lua
-            lspconfig.lua_ls.setup({
+            vim.lsp.config("lua_ls", {
                 cmd = { "lua-language-server" },
                 capabilities = capabilities,
                 settings = {
@@ -39,55 +38,44 @@ return {
             })
 
             -- TypeScript/JS
-            lspconfig.ts_ls.setup({ capabilities = capabilities })
-            lspconfig.eslint.setup({ capabilities = capabilities })
+            vim.lsp.config("ts_ls", { capabilities = capabilities })
+            vim.lsp.config("eslint", { capabilities = capabilities })
 
             -- Zig
-            lspconfig.zls.setup({ capabilities = capabilities })
+            vim.lsp.config("zls", { capabilities = capabilities })
 
             -- YAML
-            lspconfig.yamlls.setup({ capabilities = capabilities })
-
-            -- TailwindCSS
-            lspconfig.tailwindcss.setup({ capabilities = capabilities })
+            vim.lsp.config("yamlls", { capabilities = capabilities })
 
             -- Golang
-            lspconfig.gopls.setup({ capabilities = capabilities })
+            vim.lsp.config("gopls", { capabilities = capabilities })
 
             -- Nix
-            lspconfig.rnix.setup({ capabilities = capabilities })
+            vim.lsp.config("rnix", { capabilities = capabilities })
 
             -- Protocol Buffers
-            lspconfig.buf_ls.setup({ capabilities = capabilities })
-
-            -- Docker Compose
-            lspconfig.docker_compose_language_service.setup({ capabilities = capabilities })
-
-            -- Svelte
-            lspconfig.svelte.setup({ capabilities = capabilities })
-
-            -- Proto autocmd
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = "proto",
-                callback = function()
-                    lspconfig.buf_language_server.setup({ capabilities = capabilities })
-                end,
+            vim.lsp.config("buf_ls", { 
+                capabilities = capabilities,
+                filetypes = { "proto" }
             })
 
+            -- Svelte
+            vim.lsp.config("svelte", { capabilities = capabilities })
+
             -- Python
-            lspconfig.pylsp.setup({ capabilities = capabilities })
+            vim.lsp.config("pylsp", { capabilities = capabilities })
 
             -- Bash
-            lspconfig.bashls.setup({ capabilities = capabilities })
+            vim.lsp.config("bashls", { capabilities = capabilities })
 
             -- LSP keymaps
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find references" })
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 
             -- List all methods in a file
             vim.keymap.set("n", "<leader>fm", function()
@@ -101,74 +89,17 @@ return {
                     go = { "method", "struct", "interface" },
                 }
                 local symbols = symbols_map[filetype] or "function"
+                
+                -- Ensure symbols is an array for fzf-lua
+                if type(symbols) ~= "table" then
+                    symbols = { symbols }
+                end
+                
                 require("fzf-lua").lsp_document_symbols({ symbols = symbols })
-            end, {})
+            end, { desc = "Find methods" })
         end,
     },
 
-    -- Completion + Snippets
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            require("luasnip.loaders.from_vscode").lazy_load()
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-k>"] = cmp.mapping.select_prev_item(),
-                    ["<C-j>"] = cmp.mapping.select_next_item(),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.locally_jumpable(1) then
-                            luasnip.jump(1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "buffer" },
-                    { name = "path" },
-                }),
-            })
-        end,
-    },
 
     -- Java LSP
     {
@@ -176,6 +107,3 @@ return {
         ft = { "java" },
     }
 }
-
-
-
